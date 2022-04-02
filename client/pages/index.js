ccf59fc42ae4f332d2b65a4a7119bb6c1ccd7aa4
Mirko Sisko <mirko@Mirkos-MacBook-Pro.local>
2022-04-02 19:46:06 +0200

    remade project

---
+++ client/pages/index.js
@@ -0,0 +1,20 @@
+import Sidebar from '../components/Sidebar'
+
+const style = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`,
+}
+
+const Home = () => {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <Sidebar />
+        <h2>Tabs</h2>
+        <h2>Other</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
