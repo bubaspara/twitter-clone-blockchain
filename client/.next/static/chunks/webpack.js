283209f7e578f9d36cb2d4bf2d93268a46fa6013
Mirko Sisko <mirko@Mirkos-MacBook-Pro.local>
2022-04-02 20:08:31 +0200

    Added sidebar and sidebarOption components

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83f06f52ba88f790"; }
+/******/ 		__webpack_require__.h = function() { return "5495e9493781800d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,10 +1,18 @@
-// import { RiHome7Line, RiHome7Fill, RiFileList2Fill } from 'react-icons/ri'
+import { RiHome7Line, RiHome7Fill, RiFileList2Fill } from 'react-icons/ri'
 import { BiHash } from 'react-icons/bi'
 import { FiBell, FiMoreHorizontal } from 'react-icons/fi'
 import { HiOutlineMail, HiMail } from 'react-icons/hi'
 import { FaRegListAlt, FaHashtag, FaBell } from 'react-icons/fa'
 import { CgMoreO } from 'react-icons/cg'
 import { VscTwitter } from 'react-icons/vsc'
+import {
+  BsBookmarkFill,
+  BsBookmark,
+  BsPerson,
+  BsPersonFill,
+} from 'react-icons/bs'
+import { useState } from 'react'
+import SidebarOption from './SidebarOption'
 
 const style = {
   wrapper: `flex-[0.7] px-8 flex flex-col`,
@@ -21,21 +29,59 @@ const style = {
   moreContainer: `flex items-center mr-2`,
 }
 
-const Sidebar = () => {
+const Sidebar = ({ initialSelectedIcon = 'Home' }) => {
+  const [selected, setSelected] = useState(initialSelectedIcon)
   return (
     <div className={style.wrapper}>
       <div className={style.twitterIconContainer}>
         <VscTwitter />
       </div>
       <div className={style.navContainer}>
-        <div>Home</div>
-        <div>Explore</div>
-        <div>Notifications</div>
-        <div>Messages</div>
-        <div>Bookmarks</div>
-        <div>Lists</div>
-        <div>Profile</div>
-        <div>More</div>
+        <SidebarOption
+          Icon={selected === 'Home' ? RiHome7Fill : RiHome7Line}
+          text="Home"
+          isActive={Boolean(selected === 'Home')}
+          setSelected={setSelected}
+          redirect={'/'}
+        />
+        <SidebarOption
+          Icon={selected === 'Explore' ? FaHashtag : BiHash}
+          text="Explore"
+          isActive={Boolean(selected === 'Explore')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Notifications' ? FaBell : FiBell}
+          text="Notifications"
+          isActive={Boolean(selected === 'Notifications')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Messages' ? HiMail : HiOutlineMail}
+          text="Messages"
+          isActive={Boolean(selected === 'Messages')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Bookmarks' ? BsBookmarkFill : BsBookmark}
+          text="Bookmarks"
+          isActive={Boolean(selected === 'Bookmarks')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Lists' ? RiFileList2Fill : FaRegListAlt}
+          text="Lists"
+          isActive={Boolean(selected === 'Lists')}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === 'Profile' ? BsPersonFill : BsPerson}
+          text="Profile"
+          isActive={Boolean(selected === 'Profile')}
+          setSelected={setSelected}
+          redirect={'/profile'}
+        />
+        <SidebarOption Icon={CgMoreO} text="More" />
         <div className={style.tweetButton}>Mint</div>
       </div>
       <div className={style.profileButton}>
