fb3f2bea834678810e6e0ad375a4a716096adb68
Mirko Sisko <mirko.sisko@gmail.com>
2022-04-04 16:22:28 +0200

    Added Feed, TweetBox, Widgets, Post, and lib

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,6 @@
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import Feed from '../components/home/Feed'
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
@@ -10,8 +12,8 @@ const Home = () => {
     <div className={style.wrapper}>
       <div className={style.content}>
         <Sidebar />
-        <h2>Tabs</h2>
-        <h2>Other</h2>
+        <Feed />
+        <Widgets />
       </div>
     </div>
   )
